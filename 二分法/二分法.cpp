3b4598d9a1be6d11d3c91c5032ed7c7c3c27c527
wang jiahui <1042938736@qq.com>
2025-10-23 10:01:27 +0800

    二分法

---
+++ 二分法/二分法.cpp
@@ -0,0 +1,38 @@
+//¶þ·Ö·¨
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define target 20
+
+int num[] = {1,3,5,7,9,11,13,15,17,19};
+
+//×ó±ÕÓÒ±Õ
+int search(int *arr,int size,int tar)
+{
+    int left = 0,right = size - 1;
+    int mid = 0;
+    while(left <= right)
+    {
+        mid = (left+right)/2;
+        if(arr[mid] == tar)
+        return mid;
+        else if(arr[mid] < tar)
+        left = mid +1;
+        else if(arr[mid] >tar)
+        right = mid - 1;
+    }   
+    return -1; 
+}
+
+int main()
+{
+    int index = search(num,sizeof(num)/sizeof(num[0]),target);
+    if(index != -1)
+    cout <<"found target at index:"<<index<<endl;
+    else
+    cout <<"not found target"<<endl;
+    system("pause");
+    return 0;
+}
---
+++ 二分法/插入位置.cpp
@@ -0,0 +1,46 @@
+//插入位置
+//相邻LR，（L+R）/2必定为L，故插值在L右侧L=0除外
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define target 0
+
+int num[] = {1,3,5,7,9,11,13,15,17,19};
+
+//左闭右闭
+int search(int *arr,int size,int tar)
+{
+    int left = 0,right = size - 1;
+    int mid = 0;
+    while(left <= right)
+    {
+        mid = (left+right)/2;
+        if(arr[mid] == tar)
+        return mid;
+        else if(arr[mid] < tar)
+        left = mid +1;
+        else if(arr[mid] >tar)
+        right = mid - 1;
+    }   
+    return mid+1; 
+}
+
+int main()
+{
+    if(target < num[0])
+    cout <<"insert target at index:0"<<endl;
+    else
+    {
+        int index = search(num,sizeof(num)/sizeof(num[0]),target);
+        if(index != -1)
+        cout <<"found target at index:"<<index<<endl;
+        else
+        cout <<"not found target"<<endl;
+    }
+    
+    system("pause");
+    return 0;
+}
